054ef614be2918f4a93f2f8b64a559cab0e3cbbd
newtmex <brightomex@gmail.com>
2024-10-04 07:29:42 +0100

    fix build

--- packages/academy-dex-ui/app/api/bot/route.ts
+++ packages/academy-dex-ui/app/api/bot/route.ts
@@ -1,13 +1,12 @@
-import { NextRequest } from "next/server";
 import { bot } from "./bot";
 import { webhookCallback } from "grammy";
 
+export const dynamic = "force-dynamic";
+
+export const fetchCache = "force-no-store";
+
 const handler = webhookCallback(bot, "std/http");
 
-export async function GET(req: NextRequest) {
-  return await handler(req);
-}
+export const GET = handler;
 
-export async function POST(req: NextRequest) {
-  return await handler(req);
-}
+export const POST = handler;
